965683607c9609691c28141c6318a74333b0285a
MervynZhang <921255465@qq.com>
2023-12-03 00:12:26 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,7 +47,8 @@ FILE="fastproxy-darwin-${ARCH}"
 # Fetch the download URL for the latest release from GitHub
 DOWNLOAD_URL=$(curl -s https://api.github.com/repos/${REPO}/releases \
 | grep "browser_download_url.*${FILE}" \
-| cut -d '"' -f 4)
+| cut -d '"' -f 4 \
+| head -n 1)
 
 # Check if the download URL is found
 if [ -z "${DOWNLOAD_URL}" ]; then
